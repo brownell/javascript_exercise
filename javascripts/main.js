041f9b474ab1a46a0ef48af5a593a4d745d788fe
Brownell Chalstrom <brownell@chalstrom.com>
2013-07-02 00:24:08 -0500

    merged brownell code into the theme

--- javascripts/main.js
+++ javascripts/main.js
@@ -30,6 +30,28 @@ $(document).ready(function(){
   sectionHeight();
   
   $('img').load(sectionHeight);
+
+  $("#update").on('click', function(event) {
+    var repo = $("#repo").val();
+    $.ajax({
+      url: "https://api.github.com/repos/" + repo + "/commits",
+      success: function( data ) {
+        $("#commits-header").html("<br />Recent Commits for: ");
+        $("#commits-header").append(repo).show();
+        $("#commits-list").html("");
+        $.each(data, function(index, commit) {  
+          $("#commits-list").append( "<div class=\"commit\">");        
+          $("#commits-list").append("<br />sha: "+commit.sha);
+          $("#commits-list").append("<br />author email: "+commit.commit.author.email);
+          $("#commits-list").append("<br />commit message: "+commit.commit.message);
+          $("#commits-list").append("<br />date of commit: "+commit.commit.author.date);
+          $("#commits-list").append("</div>");
+          if (index == 3)
+            return(false);
+        });
+      }
+    });
+  })
 });
 
 fixScale = function(doc) {
